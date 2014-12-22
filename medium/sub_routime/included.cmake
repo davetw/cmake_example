13b9dc2437026b9999541c0605e9df5fb09098a8
Chia-Wei Chang <rglyab@gmail.com>
2014-12-22 15:01:02 +0800

    add more explanation on include() command

--- medium/sub_routime/included.cmake
+++ medium/sub_routime/included.cmake
@@ -1,1 +1,4 @@
 message("In included.cmake")
+message("The included script will not change its current directory")
+message("\tthe CMAKE_CURRENT_SOURCE_DIRECTORY = ${CMAKE_CURRENT_SOURCE_DIRECTORY}")
+message("\tthe CMAKE_CURRENT_BINARY_DIRECTORY = ${CMAKE_CURRENT_BINARY_DIRECTORY}")
